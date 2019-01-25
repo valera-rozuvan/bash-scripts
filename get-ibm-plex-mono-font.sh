ed83b6b02c0b395d2ade2091edf5f55da9c5c60a
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-01-25 20:04:25 +0200

    Create get-ibm-plex-mono-font.sh

---
+++ get-ibm-plex-mono-font.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir -p ~/temp
+cd ~/temp
+
+wget https://github.com/IBM/type/archive/master.zip
+unzip ./master.zip
+rm ./master.zip
+
+sudo mkdir -p /usr/share/fonts/truetype/ibm-plex
+sudo mkdir -p /usr/share/fonts/opentype/ibm-plex
+
+sudo cp ./plex-master/IBM-Plex-Mono/fonts/complete/ttf/*.ttf /usr/share/fonts/truetype/ibm-plex
+sudo cp ./plex-master/IBM-Plex-Mono/fonts/complete/otf/*.otf /usr/share/fonts/opentype/ibm-plex
+
+sudo fc-cache -fv
+
+exit 0
