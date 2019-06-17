0bddb0795a2f1a3d23c3eace7947161597427316
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-06-17 17:30:43 +0300

    Adding Debian setup script.

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ A collection of useful shell scripts.
 4. [Run brave browser](run-brave.sh)
 5. [Make m3u playlist](make-m3u-playlist.sh)
 6. [Get all GitHub repos](get-all-git-repos.sh)
-7. [Convert any video/audio file to MP3][convert-to-mp3.sh]
+7. [Convert any video/audio file to MP3](convert-to-mp3.sh)
 8. [Clear Gnome keyring](clear-keyring.sh)
 9. [Run emacsclient](run-emacsclient.sh)
+10. [first time Debian setup](debian-setup.sh)
 
 
 ## Other stuff out there
---
+++ debian-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt-get install aptitude
+sudo aptitude install terminator \
+  apt-transport-https \
+  curl \
+  rar \
+  unrar \
+  vlc \
+  ffmpeg \
+  mplayer \
+  git \
+  emacs25 \
+  emacs25-bin-common \
+  emacs25-common \
+  emacs25-el \
+  emacs25-common-non-dfsg \
+  ufw \
+  python3-pip \
+  fakeroot \
+  linux-source-4.9 \
+  bzip2 \
+  build-essential \
+  libncurses5-dev \
+  ncurses-dev \
+  xz-utils \
+  libssl-dev \
+  bc \
+  bison \
+  flex \
+  gcc \
+  pass \
+  ntp
+
+sudo -H pip3 install glances
+
+exit 0
