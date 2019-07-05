0f2862d877239d03fe78d1da33643e0c5003e647
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-07-05 18:11:10 +0300

    Updated get-all-git-repos.sh script.

--- get-all-git-repos.sh
+++ get-all-git-repos.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+GITHUB_TOKEN="__github_API_token_goes_here__"
+GITHUB_ORG_NAME="__github_org_name_goes_here__"
+
 curl \
-  -H 'Authorization: token {YOUR_GITHUB_TOKEN}' \
-  -s "https://api.github.com/orgs/{YOUR_ORG_NAME}/repos?page=1&per_page=100" | \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  -s "https://api.github.com/orgs/${GITHUB_ORG_NAME}/repos?page=1&per_page=100" | \
 
 grep -w clone_url | \
-grep -o '[^"]\+://.\+.git' > \
-
-repos.sh
+grep -o '[^"]\+://.\+.git' > repos.sh
 
 exit 0
