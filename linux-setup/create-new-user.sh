2e797290024fe5dbb43dbb3fff291c1d5391b4a2
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-02-04 18:07:35 +0200

    Update create-new-user.sh

--- linux-setup/create-new-user.sh
+++ linux-setup/create-new-user.sh
@@ -6,14 +6,12 @@ PUBLIC_KEY="public_key_string"
 useradd -m $USER
 usermod --shell /bin/bash $USER
 rm -rf /home/$USER/.bashrc
-cp /home/valera/.bashrc /home/$USER
+cp /root/.bashrc.user /home/$USER
 chown $USER:$USER /home/$USER/.bashrc
 mkdir /home/$USER/.ssh
 echo $PUBLIC_KEY > /home/$USER/.ssh/authorized_keys
 chown $USER:$USER /home/$USER/.ssh
 chown $USER:$USER /home/$USER/.ssh/authorized_keys
-cp /home/valera/attach.sh /home/$USER
-chown $USER:$USER /home/$USER/attach.sh
 
 echo "Done!"
 exit 0
