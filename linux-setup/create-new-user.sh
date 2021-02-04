1fdfda97f434533fe96074524b486456e57dccd7
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-02-04 18:15:04 +0200

    Update create-new-user.sh

--- linux-setup/create-new-user.sh
+++ linux-setup/create-new-user.sh
@@ -9,10 +9,10 @@ rm -rf /home/$USER/.bashrc
 
 # See example of `.bashrc.user` file here:
 #   https://github.com/valera-rozuvan/shell-script-collection/blob/master/linux-setup/.bashrc.user
-cp /root/.bashrc.user /home/$USER
+cp /root/.bashrc.user /home/$USER/.bashrc
 
 chown $USER:$USER /home/$USER/.bashrc
-mkdir /home/$USER/.ssh
+mkdir -p /home/$USER/.ssh
 echo $PUBLIC_KEY > /home/$USER/.ssh/authorized_keys
 chown $USER:$USER /home/$USER/.ssh
 chown $USER:$USER /home/$USER/.ssh/authorized_keys
