e166fc3a6ce6e384b983375b7b2ef63aa8c917a2
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-02-04 18:09:41 +0200

    Update create-new-user.sh

--- linux-setup/create-new-user.sh
+++ linux-setup/create-new-user.sh
@@ -6,7 +6,11 @@ PUBLIC_KEY="public_key_string"
 useradd -m $USER
 usermod --shell /bin/bash $USER
 rm -rf /home/$USER/.bashrc
+
+# See example of `.bashrc.user` file here:
+#   https://github.com/valera-rozuvan/shell-script-collection/blob/master/linux-setup/.bashrc.user
 cp /root/.bashrc.user /home/$USER
+
 chown $USER:$USER /home/$USER/.bashrc
 mkdir /home/$USER/.ssh
 echo $PUBLIC_KEY > /home/$USER/.ssh/authorized_keys
